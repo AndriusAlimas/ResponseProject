9eeb4083aeedd66738abac7fb3d4191c3292d83b
Andrius <andriusjavait@gmail.com>
2017-01-07 17:46:30 +0000

    Added img directory which will be one download file as well you can look at it, and added full functionallity of POST method, with all explanations

--- ResponseProject/src/com/example/servlet/MyServlet.java
+++ ResponseProject/src/com/example/servlet/MyServlet.java
@@ -43,6 +43,64 @@ public class MyServlet extends HttpServlet {
 	}
 	public void doPost(HttpServletRequest request,
 			HttpServletResponse response)throws ServletException,IOException{
-		
+			// using request object getParameter value to know if you want to download a file
+			// you get this parameter from form and store to String value to use it later
+		 	String answer = request.getParameter("download");
+		 	// just simply file name, later we will use to concatinate realPath 
+	        String file_name = "love.jpg";
+	        // using response object we set content type will be image jpeg, that we want
+	        response.setContentType("image/jpeg");
+	     
+	        int n_byte = 0;
+	        // using switch statment we check answers what to do next:
+	        switch (answer) {
+	        // download case:
+            case "download": {
+            	// this - is simply running servlet, each servlets has own servletContext object
+            	// when we get servlet context object we can do some actions with it
+            	// one of them to get realPath
+                String realPath = this.getServletContext().getRealPath(File.separator);
+                // File where should server look for:
+                File fileToDownload = new File(realPath + "/img/" + file_name);
+                
+                // HttpServletResponse interface has method addHeader(String name, String value)
+                // Adds a response header with the given name and value. This method allows
+                // response headers to have multiple values.
+                // this how we set to download file, to add in header information:
+                response.addHeader("Content-Disposition", "attachment; filename=" + file_name);
+                
+                // ServletResponse interface has setContentLength(int) method
+                // Sets the length of the content body in the response In HTTP servlets,
+                // this method sets the HTTP Content-Length header.
+                // remember from File method length() you get long, but this method required int
+                // type in argument so you have to cast it:
+                response.setContentLength((int)fileToDownload.length());
+                
+                FileInputStream in = new FileInputStream(fileToDownload);
+                /* ServletResponse interface has method getOutputStream() 
+                 Returns a ServletOutputStream suitable for writing binary
+                 data in the response. The servlet container does not encode the binary data.Calling
+                 flush() on the ServletOutputStream commits the response. Either this method or 
+                 getWriter() may be called to write the body, not both. */
+                ServletOutputStream servletOutputStream = response.getOutputStream();
+                while ((n_byte = in.read()) != -1) {
+                    servletOutputStream.write(n_byte);
+                }
+                break;
+            }
+            // if any other receive answer action happen this: 
+            default: {
+                ServletContext servletContext = this.getServletContext();
+                // as well servletContext object has method to get from resource file as byte stream
+                InputStream in = servletContext.getResourceAsStream("/img/" + file_name);
+                byte[] bytes = new byte[1024];
+                ServletOutputStream servletOutputStream = response.getOutputStream();
+                while ((n_byte = in.read(bytes)) != -1) {
+                    servletOutputStream.write(bytes, 0, n_byte);
+                }
+                servletOutputStream.flush();
+                servletOutputStream.close();
+            }
+        }
 	}
 }
